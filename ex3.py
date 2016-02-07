ef6866593e938d0aaa09ace80ce4a2e7ee537b63
Elizabeth G. Niebauer <pudgykogima@users.noreply.github.com>
2016-02-07 12:52:42 -0600

    Create ex3.py

---
+++ ex3.py
@@ -0,0 +1,25 @@
+# prints string
+print "I will now count my chickens:"
+
+# prints string followed by result of calculation (PEMDAS)
+print "Hens", 25 + 30 / 6
+print "Roosters", 100 - 25 * 3 % 4
+
+print "Now I will count the eggs:"
+
+print 3.5 + 2.25 + 1.75 - 5.5 + 4.25 % 2.75 - 1.5 / 4.25 + 6.75
+
+print "Is is true that 3 + 2 < 5 - 7?"
+
+print 3 + 2 < 5 - 7
+
+print "What is 3 + 2?", 3 + 2
+print "What is 5 - 7?", 5 - 7
+
+print "Oh, that's why it's False."
+
+print "How about some more."
+
+print "Is it greater?", 5 > -2
+print "Is it greater or equal?", 5 >= -2
+print "is it less or equal?", 5 <= -2
